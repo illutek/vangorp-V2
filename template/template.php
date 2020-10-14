f1a8081ed9aadd96ef17a3a20897226225e3f7ec
illutek <stefanvandenborne@gmail.com>
2020-10-14 10:28:14 +0200

    add google analytics  Changes to be committed: 	modified:   template/template.php

--- template/template.php
+++ template/template.php
@@ -126,4 +126,15 @@
 	});
 </script>
 
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-3878171-68"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-3878171-68');
+</script>
+
+
 </html>
