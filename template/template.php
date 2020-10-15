48ac9604bb92c27d06afbcf39cf1c1fda571b7d5
illutek <stefanvandenborne@gmail.com>
2020-10-15 10:13:13 +0200

    slider online bug jquery to 1.11.0  Changes to be committed: 	modified:   template/template.php

--- template/template.php
+++ template/template.php
@@ -90,7 +90,7 @@
 
 </body>
 
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 <script src="../dist/js/all.js"></script>
 <script>
 	$(document).ready(function() {
